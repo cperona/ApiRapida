3365c2778f1ecd08e8cc5cde0bea940b7e9b8b22
Christian Perona <cperona@jaumebalmes.net>
2023-04-24 20:26:31 +0200

    Creat controlador per a la llista de projectes. Creada una plantilla de Thymeleaf amb la llista de projectes que rep les dades des del controlador.

--- src/main/java/com/example/examplespringboot/controladors/ProjecteControlador.java
+++ src/main/java/com/example/examplespringboot/controladors/ProjecteControlador.java
@@ -1,7 +1,10 @@
 package com.example.examplespringboot.controladors;
 
 import com.example.examplespringboot.entitats.Projecte;
+import com.example.examplespringboot.repositoris.ProjecteDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 @RequestMapping("api")
 public class ProjecteControlador {
+    @Autowired
+    ProjecteDAO repositoriProjectes;
+    @GetMapping("projectes")
+    public String projectes(Model model) {
+        model.addAttribute("projectes", repositoriProjectes.findAll());
+        return "llista_projectes";
+    }
     @GetMapping("hola")
     public String holaMon() {
         return "template.html";
--- src/main/java/com/example/examplespringboot/entitats/Projecte.java
+++ src/main/java/com/example/examplespringboot/entitats/Projecte.java
@@ -11,12 +11,34 @@ public class Projecte {
     private String nom;
     private String estat;
 
-    public long getId() {
-        return this.id;
+    public Projecte() {
     }
-
     public Projecte(String nom, String estat) {
         this.nom = nom;
+        this.estat = estat;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEstat() {
+        return estat;
+    }
+
+    public void setEstat(String estat) {
         this.estat = estat;
     }
 }
